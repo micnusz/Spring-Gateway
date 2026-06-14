32266c1b1129b302c56d1cc6c631421814088f9d
Michal <michalnuszkiewiczbusiness@gmail.com>
2026-06-14 14:08:48 +0200

    Added docker-compose

--- gateway/src/main/java/com/micnusz/gateway/Controller/GatewayController.java
+++ gateway/src/main/java/com/micnusz/gateway/Controller/GatewayController.java
@@ -1,14 +1,14 @@
-package com.micnusz.roleService.Controller;
+package com.micnusz.gateway.Controller;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class RoleController {
+public class GatewayController {
 
     @GetMapping("/message")
     public String message() {
-        return "RoleController message";
+        return "GatewayController message";
     }
 }
